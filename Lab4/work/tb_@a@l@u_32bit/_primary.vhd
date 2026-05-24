37697c9c3dcf4a234d5904298423401f12f2bfcb
NguyenKhanhToan21 <89009538+NguyenKhanhToan21@users.noreply.github.com>
2026-05-24 18:18:07 +0700

    Delete Lab4 directory

--- Lab4/work/tb_@a@l@u_32bit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_ALU_32bit is
-end tb_ALU_32bit;
--- Lab4/work/tb_single_port_@r@a@m/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_single_port_RAM is
-end tb_single_port_RAM;
